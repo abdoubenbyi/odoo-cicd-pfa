6fb97ef5b9cf63bf55306ba48e311bea3e51b02b
ilyasZAOUIA <ilyaszaouiapro1@gmail.com>
2026-07-07 17:51:28 +0100

    Test addons

---
+++ custom_addons/pipeline_demo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Pipeline Demo",
+    "version": "17.0.1.0.0",
+    "summary": "Demo module for CI/CD testing",
+    "author": "ilyas",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/demo_views.xml",
+        "data/demo_data.xml",
+    ],
+    "application": True,
+}
